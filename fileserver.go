6ea2b77572ca67d6535a68d96622df0c82455b56
Joe Potter <JDPotter@gmail.com>
2015-06-22 02:19:45 -0400

    pause button, cycles 10 sec

--- fileserver.go
+++ fileserver.go
@@ -18,7 +18,8 @@ var GameList []forms.Games
 var StreamerList []forms.Streamers
 
 type Streamer struct {
-	Name string
+	Name    string
+	Viewers int
 }
 
 func main() {
@@ -36,19 +37,21 @@ func main() {
 }
 
 func Twitch(w http.ResponseWriter, r *http.Request) {
-	var streamer = StreamerList[rand.Intn(len(StreamerList))].Name
-	ExeTemplate(streamer, w, r)
+	var ran = rand.Intn(len(StreamerList))
+	fmt.Println("Fetching " + StreamerList[ran].Name)
+	ExeTemplate(StreamerList[ran].Name, StreamerList[ran].Viewers, w, r)
 }
 
 func Next(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getting another random streamer")
-	var streamer = StreamerList[rand.Intn(len(StreamerList))].Name
-	fmt.Println("fetching " + streamer)
-	ExeTemplate(streamer, w, r)
+	fmt.Println("Fetching new streamer list...")
+	GetStreams()
+	var ran = rand.Intn(len(StreamerList))
+	fmt.Println("Fetching " + StreamerList[ran].Name)
+	ExeTemplate(StreamerList[ran].Name, StreamerList[ran].Viewers, w, r)
 }
 
-func ExeTemplate(name string, w http.ResponseWriter, r *http.Request) {
-	stre := Streamer{name}
+func ExeTemplate(name string, viewers int, w http.ResponseWriter, r *http.Request) {
+	stre := Streamer{name, viewers}
 
 	fp := path.Join("templates", "index.html")
 	tmpl, err := template.ParseFiles(fp)
@@ -68,9 +71,12 @@ func GetStreams() forms.StreamS {
 	var Streams forms.StreamS
 	var offset int
 
+	StreamerList = make([]forms.Streamers, 0)
+	GameList = make([]forms.Games, 0)
+
 	baseURL := "https://api.twitch.tv/kraken"
 
-	resp, err := http.Get(baseURL + "/streams?limit=100")
+	resp, err := http.Get(baseURL + "/streams?game=Hearthstone%3A+Heroes+of+Warcraft")
 	defer resp.Body.Close()
 	if err != nil {
 		panic(err)
@@ -83,13 +89,16 @@ func GetStreams() forms.StreamS {
 		panic(err)
 	}
 
-	numt := int(float64(SomeStreams.Total)*.05) / 100 //number of times we need to get max limit (100) GETs
-	numm := int(float64(SomeStreams.Total)*.05) % 100 //number to pick up the remainder of limit for GET (will be limit=numm)
-	var skip = SomeStreams.Total - int(float64(SomeStreams.Total)*.05)
+	//numt := int(float64(SomeStreams.Total)*.05) / 100 //number of times we need to get max limit (100) GETs
+	//numm := int(float64(SomeStreams.Total)*.05) % 100 //number to pick up the remainder of limit for GET (will be limit=numm)
+	//var skip = SomeStreams.Total - int(float64(SomeStreams.Total)*.05)
+	numt := SomeStreams.Total / 100
+	numm := SomeStreams.Total % 100
+	fmt.Println("Found " + strconv.Itoa(SomeStreams.Total) + " streamers.")
 
 	for i := 0; i < numt; i++ {
 		offset = 100 * i
-		resp, err := http.Get(baseURL + "/streams?limit=100&offset=" + strconv.Itoa(offset+skip))
+		resp, err := http.Get(baseURL + "/streams?limit=100&game=Hearthstone%3A+Heroes+of+Warcraft&offset=" + strconv.Itoa(offset))
 		defer resp.Body.Close()
 		if err != nil {
 			panic(err)
@@ -108,7 +117,7 @@ func GetStreams() forms.StreamS {
 	offset = numt * 100
 
 	if numm > 0 { //if there are any remaining streams to get
-		resp, err := http.Get(baseURL + "/streams?limit=" + strconv.Itoa(numm) + "&offset=" + strconv.Itoa(offset+skip))
+		resp, err := http.Get(baseURL + "/streams?limit=" + strconv.Itoa(numm) + "&game=Hearthstone%3A+Heroes+of+Warcraft&offset=" + strconv.Itoa(offset))
 		if err != nil {
 			panic(err)
 		}
@@ -140,7 +149,9 @@ func BuildStreamList(Streams forms.StreamS, SomeStreams forms.StreamS) forms.Str
 		tempStream.Viewers = tempGame.Viewers
 		tempStream.Name = SomeStreams.Streams[i].Channel.Name
 		tempStream.Game = SomeStreams.Streams[i].Game
+		//if tempStream.Viewers > 0 {
 		StreamerList = append(StreamerList, tempStream)
+		//}
 
 		for j := 0; j < len(GameList); j++ {
 			if GameList[j].Name == tempGame.Name {
